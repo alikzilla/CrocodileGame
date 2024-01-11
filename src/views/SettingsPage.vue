102ea67500bcf9a3c4108780312ddb63d1f6cf33
alikzilla <nuspekovalihan@gmail.com>
2024-01-11 23:47:25 +0600

    tabs bar and some tabs

---
+++ src/views/SettingsPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>Settings</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+</script>
+
+<style scoped>
+ion-title {
+  text-align: center;
+}
+</style>
